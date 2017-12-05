a760c091e46f43bafd2f18812d73728fd3f9cffc
Shin1122 <big_shin@126.com>
2017-12-05 23:31:30 +0800

    Initial Commit

---
+++ Weibo_Imitate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Weibo_Imitate
+//
+//  Created by Shin on 2017/12/5.
+//  Copyright © 2017年 Shin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
